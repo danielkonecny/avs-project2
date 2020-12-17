5b9794e4fd6e69670f4bd2ec7ea28c2bbc51983b
Daniel Konecny <dkonecny97@gmail.com>
2020-12-17 02:06:18 +0100

    Task 1 and 2 done

--- src/parallel_builder/loop_mesh_builder.h
+++ src/parallel_builder/loop_mesh_builder.h
@@ -14,6 +14,9 @@
 #include <vector>
 #include "base_mesh_builder.h"
 
+/**
+ * @brief The LoopMeshBuilder class
+ */
 class LoopMeshBuilder : public BaseMeshBuilder
 {
 public:
@@ -23,7 +26,9 @@ protected:
     unsigned marchCubes(const ParametricScalarField &field);
     float evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field);
     void emitTriangle(const Triangle_t &triangle);
-    const Triangle_t *getTrianglesArray() const { return nullptr; }
+    const Triangle_t *getTrianglesArray() const { return mTriangles.data(); }
+
+    std::vector<Triangle_t> mTriangles; ///< Temporary array of triangles
 };
 
 #endif // LOOP_MESH_BUILDER_H
--- src/parallel_builder/tree_mesh_builder.h
+++ src/parallel_builder/tree_mesh_builder.h
@@ -11,18 +11,25 @@
 #ifndef TREE_MESH_BUILDER_H
 #define TREE_MESH_BUILDER_H
 
+#include <vector>
 #include "base_mesh_builder.h"
 
+/**
+ * @brief The TreeMeshBuilder class
+ */
 class TreeMeshBuilder : public BaseMeshBuilder
 {
 public:
     TreeMeshBuilder(unsigned gridEdgeSize);
 
 protected:
+	unsigned treeDive(const ParametricScalarField &field, Vec3_t<float> position, unsigned depth);
     unsigned marchCubes(const ParametricScalarField &field);
     float evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field);
     void emitTriangle(const Triangle_t &triangle);
-    const Triangle_t *getTrianglesArray() const { return nullptr; }
+    const Triangle_t *getTrianglesArray() const { return mTriangles.data(); }
+
+    std::vector<Triangle_t> mTriangles; ///< Temporary array of triangles
 };
 
 #endif // TREE_MESH_BUILDER_H
